1630c598959e4aeeadc6e1abd2202e699197f2c8
Martin Kearn <martin.kearn@microsoft.com>
2022-04-27 15:45:13 +0100

    Added basic project structure, placeholder readme, gitignore and project #2

---
+++ src/mock-classifier.integration-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace mock_classifier.integration_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/mock-classifier.unit-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace mock_classifier.unit_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
