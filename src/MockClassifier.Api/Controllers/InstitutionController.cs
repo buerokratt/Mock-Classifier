56c33e33c57f11887f89e7575de4d9bcc750bde0
Martin Kearn <martin.kearn@microsoft.com>
2022-05-10 11:01:45 +0100

    Institutions controller scaffolding

---
+++ src/MockClassifier.Api/Controllers/InstitutionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockClassifier.Api.Models;
+
+namespace MockClassifier.Api.Controllers
+{
+    [Route("input-from-dmr/institution")]
+    [ApiController]
+    public class InstitutionController : ControllerBase
+    {
+        [HttpPost]
+        public AcceptedResult Post([FromBody] MessagesInput messages)
+        {
+            return Accepted();
+        }
+    }
+}
