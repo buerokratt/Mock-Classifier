bf5b71724079c6c0637ce7489cd1bceda189d9cb
Martin Kearn <martin.kearn@microsoft.com>
2022-05-10 11:05:29 +0100

    Controller comments

--- src/MockClassifier.Api/Controllers/InstitutionController.cs
+++ src/MockClassifier.Api/Controllers/InstitutionController.cs
@@ -7,6 +7,11 @@ namespace MockClassifier.Api.Controllers
     [ApiController]
     public class InstitutionController : ControllerBase
     {
+        /// <summary>
+        /// Processes an array of strings to identify corresponding ministries and issues call backs to DMR for each message.
+        /// </summary>
+        /// <param name="messages">Property which contains an array of strings representing a user message in each string</param>
+        /// <returns>An empty 202/Accepted result</returns>
         [HttpPost]
         public AcceptedResult Post([FromBody] MessagesInput messages)
         {
