2f421faa7e3e1815ac5deadf36dac7a6c941193e
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-05-13 11:17:15 +0100

    MClassifier: Creating a service that calls back to the DMR API (#25)
    * DMR Service implementation
    
    * Unit tests and some refactoring
    
    * Reverting some changes
    
    * Some thing missed in the revert
    
    * Remove default DmrApiUri
    Add CONTRIBUTING.md
    
    * Removed duplicate instance of a string

---
+++ src/MockClassifier.Api/Services/Dmr/DmrHostedService.cs
@@ -0,0 +1,65 @@
+﻿namespace MockClassifier.Api.Services.Dmr
+{
+    /// <summary>
+    /// A background hosted service that periodically triggers the DMR request processor
+    /// </summary>
+    public class DmrHostedService : IHostedService
+    {
+        private readonly IDmrService dmrService;
+        private readonly DmrServiceSettings config;
+        private readonly ILogger<DmrHostedService> logger;
+        private readonly Timer timer;
+
+        public DmrHostedService(IDmrService dmrService, DmrServiceSettings config, ILogger<DmrHostedService> logger)
+        {
+            this.dmrService = dmrService;
+            this.config = config;
+            this.logger = logger;
+            this.timer = new Timer(TimerCallback, this, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            StartTimer();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            StopTimer();
+
+            return Task.CompletedTask;
+        }
+
+        private void StopTimer() => timer.Change(Timeout.Infinite, Timeout.Infinite);
+        private void StartTimer() => timer.Change(TimeSpan.FromMilliseconds(config.DmrRequestProcessIntervalMs), Timeout.InfiniteTimeSpan);
+
+        private static async void TimerCallback(object state)
+        {
+            if (state == null)
+                return;
+
+            var self = state as DmrHostedService;
+
+            self.StopTimer();
+
+            try
+            {
+                self.logger.LogInformation("Starting processing DMR requests");
+
+                await self.dmrService.ProcessRequestsAsync();
+
+                self.logger.LogInformation("Completed processing DMR requests");
+            }
+            catch (Exception ex)
+            {
+                self.logger.LogError(ex, $"Unexpected error in {nameof(DmrHostedService)}.");
+            }
+            finally
+            {
+                self.StartTimer();
+            }
+        }
+    }
+}
---
+++ src/MockClassifier.UnitTests/Services/Dmr/DmrServiceTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Logging;
+using MockClassifier.Api.Services.Dmr;
+using MockClassifier.UnitTests.Extensions;
+using Moq;
+using RichardSzalay.MockHttp;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockClassifier.UnitTests.Services.Dmr
+{
+    public class DmrServiceTests
+    {
+        private static readonly DmrServiceSettings DefaultServiceConfig = new DmrServiceSettings
+        {
+            DmrApiUri = "https://dmr.fakeurl.com"
+        };
+
+        private readonly Mock<IHttpClientFactory> httpClientFactory = new Mock<IHttpClientFactory>();
+        private readonly MockHttpMessageHandler httpMessageHandler = new MockHttpMessageHandler();
+        private readonly Mock<ILogger<DmrService>> logger = new Mock<ILogger<DmrService>>();
+
+        [Fact]
+        public async Task ShouldCallDmrApiWithGivenRequest_WhenRequestIsRecorded()
+        {
+            httpMessageHandler.SetupWithMessage();
+
+            var clientFactory = GetHttpClientFactory(httpMessageHandler);
+
+            var sut = new DmrService(clientFactory.Object, DefaultServiceConfig, logger.Object);
+
+            sut.RecordRequest(GetDmrRequest());
+
+            await sut.ProcessRequestsAsync();
+
+            httpMessageHandler.VerifyNoOutstandingExpectation();
+        }
+
+        [Fact]
+        public async Task ShouldCallDmrApiForEachGivenRequest_WhenMultipleRequestsAreRecorded()
+        {
+            httpMessageHandler
+                .SetupWithMessage("my first message")
+                .SetupWithMessage("my second message");
+            
+            var clientFactory = GetHttpClientFactory(httpMessageHandler);
+
+            var sut = new DmrService(clientFactory.Object, DefaultServiceConfig, logger.Object);
+            
+            sut.RecordRequest(GetDmrRequest("my first message"));
+            sut.RecordRequest(GetDmrRequest("my second message"));
+
+            await sut.ProcessRequestsAsync();
+
+            httpMessageHandler.VerifyNoOutstandingExpectation();
+        }
+
+        [Fact]
+        public async Task ShouldNotThrowException_WhenCallToDmrApiErrors()
+        {
+            var dmrHttpClient = new MockHttpMessageHandler();
+            dmrHttpClient.When("/").Respond(HttpStatusCode.BadGateway);
+
+            var clientFactory = GetHttpClientFactory(dmrHttpClient);
+
+            var sut = new DmrService(clientFactory.Object, DefaultServiceConfig, logger.Object);
+
+            sut.RecordRequest(GetDmrRequest());
+
+            await sut.ProcessRequestsAsync();
+        }
+
+        private Mock<IHttpClientFactory> GetHttpClientFactory(MockHttpMessageHandler messageHandler, DmrServiceSettings settings = null)
+        {
+            settings = settings ?? DefaultServiceConfig;
+
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(m => m.CreateClient(It.IsAny<string>()))
+                .Returns(() =>
+                {
+                    var client = messageHandler.ToHttpClient();
+                    client.BaseAddress = new Uri(settings.DmrApiUri);
+
+                    return client;
+                });
+
+            return mockHttpClientFactory;
+        }
+
+
+        private DmrRequest GetDmrRequest(string message = "my test message")
+        {
+            return new DmrRequest
+            {
+                ForwardUri = "https://forwarduri.fakeurl.com",
+                Payload = new Payload
+                {
+                    CallbackUri = "https://callbackuri.fakeurl.com",
+                    Messages = new[] { message },
+                    Ministry = "border"
+                }
+            };
+        }
+    }
+}
