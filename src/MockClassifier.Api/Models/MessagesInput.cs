cf6113c7d2986ab9fffb23735be49d143451d50d
Martin Kearn <martin.kearn@microsoft.com>
2022-05-13 16:51:44 +0100

    Mock Classifier CI Pipeline (#27)
    * Added template .net pipeline
    
    * Removed branch restriction
    
    * Typo
    
    * .net version 6
    
    * Specify solution
    
    * Spacing steps out for readability
    
    * Upgrade nuget pacjages
    
    * Added  --collect:"XPlat Code Coverage"
    
    * Publish coverage report
    
    * Set copy coverage path
    
    * Added results-directory
    
    * Remove third party code coverage extensions
    
    * Added CI status badge
    
    * Added dotnet format and wildcard for sln
    
    * Typo on format
    
    * Added Cobertura Coverage output to root
    
    * Change test proj path
    
    * Remove leading backslash
    
    * Added code coverage steps
    
    * Comments and wildcard in test project path
    
    * Mutation tests
    
    * Added Stryker
    
    * dotnet tool restore
    
    * Set working directory
    
    * Go up one level in path
    
    * Copy coverage to src
    
    * Separate defaults and new path for coverage file
    
    * Separate jobs
    
    * Single job
    
    * Set tool-manifest
    
    * Move to unit test folder as per docs
    
    * Set working directory for stryker
    
    * src as working directory
    
    * Move stryker file to solution level
    
    * set path for stryker report
    
    * Set coverage threshold to 80
    
    * Remove pipeline on push
    
    * Upgrade packages
    
    * Added some [ExcludeFromCodeCoverage] to pass coverage
    
    * Upload coverage report
    
    * Upload coverage xml
    
    * Change sequence of steps
    
    * Set Stryker thresholds
    
    * Added cleartext reporter
    
    * Added comments for code coverage exclusions
    
    * Remove redundant comment
    
    * more descriptive filename

--- src/MockClassifier.Api/Models/MessagesInput.cs
+++ src/MockClassifier.Api/Models/MessagesInput.cs
@@ -1,5 +1,9 @@
-﻿namespace MockClassifier.Api.Models
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MockClassifier.Api.Models
 {
+    // No logic so no unit tests are required
+    [ExcludeFromCodeCoverage]
     public class MessagesInput
     {
         public string CallbackUri { get; set; }
