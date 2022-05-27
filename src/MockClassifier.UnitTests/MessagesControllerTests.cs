033ff622946002bc3e858a8e38f28baef3792fc4
Martin Kearn <martin.kearn@microsoft.com>
2022-05-27 16:42:28 +0100

    ReturnsAccepted passing

--- src/MockClassifier.UnitTests/MessagesControllerTests.cs
+++ src/MockClassifier.UnitTests/MessagesControllerTests.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using MockClassifier.Api.Controllers;
 using MockClassifier.Api.Services;
 using MockClassifier.Api.Services.Dmr;
 using Moq;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace MockClassifier.UnitTests
 {
-    public class InstitutionControllerTests
+    public class MessagesControllerTests
     {
         private readonly MessagesController sut;
         private readonly Mock<IDmrService> dmrService = new();
@@ -16,7 +20,7 @@ namespace MockClassifier.UnitTests
         private readonly NaturalLanguageService naturalLanguageService = new();
         private readonly EncodingService encodingService = new();
 
-        public InstitutionControllerTests()
+        public MessagesControllerTests()
         {
             dmrService = new Mock<IDmrService>();
             sut = new MessagesController(dmrService.Object, tokenService, naturalLanguageService, encodingService);
@@ -27,11 +31,18 @@ namespace MockClassifier.UnitTests
         [InlineData("message1")]
         public async Task ReturnsAccepted(string message)
         {
-            // Arrange
-            var request = new DmrRequestPayload
+            // Arrange payload
+            var payload = new DmrRequestPayload
             {
                 Message = message,
                 Classification = string.Empty
+            };
+            var encodedPayload = encodingService.EncodeBase64(JsonSerializer.Serialize(payload));
+
+            // Arrange controller with context
+            sut.ControllerContext = new ControllerContext()
+            {
+                HttpContext = GetContext(encodedPayload)
             };
 
             _ = dmrService.Setup(m => m.RecordRequest(It.IsAny<DmrRequest>()));
@@ -63,6 +74,15 @@ namespace MockClassifier.UnitTests
             // Assert
             _ = Assert.IsType<AcceptedResult>(result);
             dmrService.Verify(x => x.RecordRequest(It.IsAny<DmrRequest>()), Times.Once());
+        }
+
+        private static DefaultHttpContext GetContext(string payload)
+        {
+            var httpContext = new DefaultHttpContext();
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(payload));
+            httpContext.Request.Body = stream;
+            httpContext.Request.ContentLength = stream.Length;
+            return httpContext;
         }
     }
 }
